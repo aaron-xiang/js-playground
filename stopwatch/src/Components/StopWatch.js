7ed207ea81750eb27af59ec2a4343e23ebb0b27d
Henry Xiang <hxxiang@ucdavis.edu>
2020-08-06 18:07:31 -0700

    updated stopwatch

--- stopwatch/src/Components/StopWatch.js
+++ stopwatch/src/Components/StopWatch.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 
 const styles = {
-  container: {},
+  container: {
+    border: "2px solid blue",
+    margin: "auto",
+    width: "150px"
+  },
   display: {},
   control: {},
 };
+
+function pad(num, size) {
+  var s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
+}
 
 export class StopWatch extends Component {
   state = {
     time: 0, //ms
     isStarted: false,
   };
+  startTime = null;
   interval = null;
   handleClick = () => {
     const { isStarted } = this.state;
@@ -21,8 +32,10 @@ export class StopWatch extends Component {
     }
   };
   start = () => {
+    this.startTime = Date.now();
     this.interval = setInterval(() => {
-      this.setState({ time: this.state.time + 1, isStarted: true });
+      const time = Date.now() - this.startTime;
+      this.setState({ time, isStarted: true });
     }, 1);
   };
   stop = () => {
@@ -33,12 +46,20 @@ export class StopWatch extends Component {
     clearInterval(this.interval);
     this.setState({ time: 0, isStarted: false });
   };
+
   hour = (time) => Math.floor(time / 3600 / 1000);
+  min = (time) => Math.floor(time / 60 / 1000) % 60;
+  sec = (time) => Math.floor(time / 1000) % 60;
+  milli = (time) => time % 1000;
   render() {
     const { time, isStarted } = this.state;
+    const hh = this.hour(time);
+    const mm = pad(this.min(time), 2);
+    const ss = pad(this.sec(time), 2);
+    const ms = pad(this.milli(time), 3);
     return (
       <div style={styles.container}>
-        <div style={styles.display}>{time}</div>
+        <div style={styles.display}>{`${hh}:${mm}:${ss}:${ms}`}</div>
         <div style={styles.control}>
           <button onClick={this.handleClick}>
             {isStarted ? "Stop" : "Start"}
